2f0ab386a3851d9ebd0383fabcef6198fea263fd
Antonina Grebeniuk <rebel.t2808@gmail.com>
2024-06-15 18:03:45 +0400

    add state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
-import { Heading } from 'components';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { lazy, useEffect } from 'react';
+import { Header } from 'components';
+import { useDispatch } from 'react-redux';
+import { fetchBaseCurrency } from './redux/currency/operations';
+import { setBaseCurrency } from './redux/currency/currencySlice';
+
+const Home = lazy(() => import('pages/Home'));
+const Rates = lazy(() => import('pages/Rates'));
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    const success = ({ coords }) => {
+      dispatch(fetchBaseCurrency(coords));
+    };
+
+    const error = () => {
+      dispatch(setBaseCurrency('USD'));
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, [dispatch]);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="/rates" element={<Rates />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
