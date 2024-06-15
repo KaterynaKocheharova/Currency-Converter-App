8fd6f52fb3947aa5daa2c7e1265b6f3d12118a36
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-06-15 15:22:33 +0300

    Revert "get iso_code"

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,5 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { lazy, useEffect } from 'react';
-import { Header } from 'components';
-import { getUserInfo } from './service';
-
-const Home = lazy(() => import('pages/Home'));
-const Rates = lazy(() => import('pages/Rates'));
+import { Heading } from 'components';
 
 export const App = () => {
-  useEffect(() => {
-navigator.geolocation.getCurrentPosition(position => {
-  const latitude = position.coords.latitude;
-  const longitude = position.coords.longitude;
-  getUserInfo({latitude, longitude});
-})
-  }, [])
-
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<Home/>}  />
-          <Route path="/rates" element={<Rates/>} />
-          <Route path="*" element={<Navigate to="/"/>}/>
-        </Route>
-      </Routes>
-    </>
-  );
+  return <Heading title="Just do it!" />;
 };
